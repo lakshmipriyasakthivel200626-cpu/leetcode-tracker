5a83199f0ea9194e089c6eefc7b1de49bb0ff8b9
lakshmipriyasakthivel200626-cpu <lakshmipriyasakthivel200626@gmail.com>
2026-07-09 09:52:40 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 9:50:39 AM
+class Solution {
+    public boolean isUgly(int n) {
+        if(n <= 0) return false; 
+        while(n > 1){
+            if(n % 2 == 0){
+                n /= 2;
+            } else if(n % 3 == 0){
+                n /= 3;
+            } else if(n % 5 == 0){
+                n /= 5;
+            } else {
+                return false; 
+            }
+        } 
+        return true; 
+    }
+}
