e90684a31348e04673a671023680a4fa242c8bfc
lakshmipriyasakthivel200626-cpu <lakshmipriyasakthivel200626@gmail.com>
2026-07-09 09:52:57 +0530

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/205-IsomorphicStrings.java
@@ -0,0 +1,11 @@
+// Last updated: 7/9/2026, 9:50:56 AM
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.indexOf(s.charAt(i)) != t.indexOf(t.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
