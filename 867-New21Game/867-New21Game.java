d970d45ee99a5e807b3318731f1dcfcad0a18af2
lakshmipriyasakthivel200626-cpu <lakshmipriyasakthivel200626@gmail.com>
2026-07-09 09:51:30 +0530

    Create 867-New21Game

---
+++ 867-New21Game/867-New21Game.java
@@ -0,0 +1,27 @@
+// Last updated: 7/9/2026, 9:49:29 AM
+class Solution {
+    public double new21Game(int n, int k, int maxPts) {
+        if (k == 0 || n >= k + maxPts) return 1.0;
+
+        double[] dp = new double[n + 1];
+        dp[0] = 1.0;
+
+        double windowSum = 1.0; 
+        double result = 0.0;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = windowSum / maxPts;
+
+            if (i < k) {
+                windowSum += dp[i];  
+            } else {
+                result += dp[i];    
+            }
+
+            if (i - maxPts >= 0) {
+                windowSum -= dp[i - maxPts];
+            }
+        }
+        return result;
+    }
+}
