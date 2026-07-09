41463ff9e71dbf486e813206332ee1a499fa5354
lakshmipriyasakthivel200626-cpu <lakshmipriyasakthivel200626@gmail.com>
2026-07-09 09:52:21 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 9:50:19 AM
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int []cnt=new int[26];
+     
+        for(int i=0;i<magazine.length();i++){
+       char c=magazine.charAt(i);
+       cnt[c-'a']++;        
+     }  
+     for(int i=0;i<ransomNote.length();i++){
+        char c=ransomNote.charAt(i);
+        if(cnt [c-'a']==0){
+          return false;
+
+        }
+        cnt[c-'a'] --;
+          
+     } 
+     return true;
+    }
+}
